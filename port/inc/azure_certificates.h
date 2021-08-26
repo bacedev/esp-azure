666b09da9a5d85bd1350cc3145fb21858300c7fb
Floris Romeijn <floris.romeijn@evalan.com>
2021-08-26 08:31:52 +0200

    for IoT Azure connection updated certificates add DigiCert Assured ID Root G2 to certificates also renamed certs.h to azure_certificates.h to prevent using the azure-iot-sdk-c certs var

--- port/inc/azure_certificates.h
+++ port/inc/azure_certificates.h
@@ -1,18 +1,18 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#ifndef CERTS_H
-#define CERTS_H
+#ifndef AZURE_CERTIFICATES_H
+#define AZURE_CERTIFICATES_H
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-    extern const char certificates[];
+    extern const char azure_certificates[];
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* CERTS_H */
+#endif /* AZURE_CERTIFICATES_H */
--- port/src/azure_certificates.c
+++ port/src/azure_certificates.c
@@ -4,10 +4,33 @@
 /* This file contains minimalistic cert needed to communicate with Azure (IoT) */
 /* The cert from this file is used in case of azure connection on esp8266 */
 
-#include "certs.h"
+#include "azure_certificates.h"
 
-const char certificates[] =
-/* DigiCert Baltimore Root */
+const char azure_certificates[] =
+/* DigiCert Assured ID Root G2 - Valid until: 15/Jan/2038 */
+"-----BEGIN CERTIFICATE-----\r\n"
+"MIIDljCCAn6gAwIBAgIQC5McOtY5Z+pnI7/Dr5r0SzANBgkqhkiG9w0BAQsFADBl\r\n"
+"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\r\n"
+"d3cuZGlnaWNlcnQuY29tMSQwIgYDVQQDExtEaWdpQ2VydCBBc3N1cmVkIElEIFJv\r\n"
+"b3QgRzIwHhcNMTMwODAxMTIwMDAwWhcNMzgwMTE1MTIwMDAwWjBlMQswCQYDVQQG\r\n"
+"EwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGlnaWNl\r\n"
+"cnQuY29tMSQwIgYDVQQDExtEaWdpQ2VydCBBc3N1cmVkIElEIFJvb3QgRzIwggEi\r\n"
+"MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDZ5ygvUj82ckmIkzTz+GoeMVSA\r\n"
+"n61UQbVH35ao1K+ALbkKz3X9iaV9JPrjIgwrvJUXCzO/GU1BBpAAvQxNEP4Htecc\r\n"
+"biJVMWWXvdMX0h5i89vqbFCMP4QMls+3ywPgym2hFEwbid3tALBSfK+RbLE4E9Hp\r\n"
+"EgjAALAcKxHad3A2m67OeYfcgnDmCXRwVWmvo2ifv922ebPynXApVfSr/5Vh88lA\r\n"
+"bx3RvpO704gqu52/clpWcTs/1PPRCv4o76Pu2ZmvA9OPYLfykqGxvYmJHzDNw6Yu\r\n"
+"YjOuFgJ3RFrngQo8p0Quebg/BLxcoIfhG69Rjs3sLPr4/m3wOnyqi+RnlTGNAgMB\r\n"
+"AAGjQjBAMA8GA1UdEwEB/wQFMAMBAf8wDgYDVR0PAQH/BAQDAgGGMB0GA1UdDgQW\r\n"
+"BBTOw0q5mVXyuNtgv6l+vVa1lzan1jANBgkqhkiG9w0BAQsFAAOCAQEAyqVVjOPI\r\n"
+"QW5pJ6d1Ee88hjZv0p3GeDgdaZaikmkuOGybfQTUiaWxMTeKySHMq2zNixya1r9I\r\n"
+"0jJmwYrA8y8678Dj1JGG0VDjA9tzd29KOVPt3ibHtX2vK0LRdWLjSisCx1BL4Gni\r\n"
+"lmwORGYQRI+tBev4eaymG+g3NJ1TyWGqolKvSnAWhsI6yLETcDbYz+70CjTVW0z9\r\n"
+"B5yiutkBclzzTcHdDrEcDcRjvq30FPuJ7KJBDkzMyFdA0G4Dqs0MjomZmWzwPDCv\r\n"
+"ON9vvKO+KSAnq3T/EyJ43pdSVR6DtVQgA+6uwE9W3jfMw3+qBCe703e4YtsXfJwo\r\n"
+"IhNzbM8m9Yop5w==\r\n"
+"-----END CERTIFICATE-----\r\n"
+/* DigiCert Baltimore Root - Valid until: 12/May/2025 */
 "-----BEGIN CERTIFICATE-----\r\n"
 "MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ\r\n"
 "RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD\r\n"
@@ -28,4 +51,5 @@ const char certificates[] =
 "Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz\r\n"
 "ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS\r\n"
 "R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp\r\n"
-"-----END CERTIFICATE-----\r\n";
+"-----END CERTIFICATE-----\r\n"
+;
